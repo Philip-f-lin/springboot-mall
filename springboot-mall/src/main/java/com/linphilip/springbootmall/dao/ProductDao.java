1bda03f3329acca301a71dc4900af5d4e18415c0
linzhuofei <philip.f.lin@gmail.com>
2023-09-09 11:40:53 +0800

    「商品功能 - 實作新增商品」

--- springboot-mall/src/main/java/com/linphilip/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/linphilip/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.linphilip.springbootmall.dao;
 
+import com.linphilip.springbootmall.dto.ProductRequest;
 import com.linphilip.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
