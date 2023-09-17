741a7c940d5de22c154c3091ad4775c4c25ffa2b
linzhuofei <philip.f.lin@gmail.com>
2023-09-17 19:42:31 +0800

    商品功能 - 實作查詢商品列表（查詢條件）

--- springboot-mall/src/main/java/com/linphilip/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/linphilip/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.linphilip.springbootmall.service.impl;
 
+import com.linphilip.springbootmall.constant.ProductCategory;
 import com.linphilip.springbootmall.dao.ProductDao;
 import com.linphilip.springbootmall.dto.ProductRequest;
 import com.linphilip.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
